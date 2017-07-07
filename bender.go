8c588cf7e2dc5e1020f13f3886527256c6583074
Raphael Deem <raphael.deem@ticketmaster>
2017-07-06 21:17:10 -0700

    first commit

---
+++ bender.go
@@ -0,0 +1,132 @@
+/*
+
+mybot - Illustrative Slack bot in Go
+
+Copyright (c) 2015 RapidLoop
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: mybot slack-bot-token\n")
+		os.Exit(1)
+	}
+	ticker := time.NewTicker(60 * time.Second)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				// do stuff
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+	// start a websocket-based Real Time API session
+	ws, id := slackConnect(os.Args[1])
+	fmt.Println("mybot ready, ^C exits")
+
+	for {
+		// read each incoming message
+		m, err := getMessage(ws)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// see if we're mentioned
+		if m.Type == "message" && strings.HasPrefix(m.Text, "<@"+id+">") {
+			// if so try to parse if
+			parts := strings.Fields(m.Text)
+			if len(parts) == 3 && parts[1] == "stock" {
+				// looks good, get the quote and reply with the result
+				go func(m Message) {
+					m.Text = getQuote(parts[2])
+					postMessage(ws, m)
+				}(m)
+				// NOTE: the Message object is copied, this is intentional
+			} else if parts[1] == "gif" && parts[2] == "me" {
+				go func(m Message) {
+					m.Text = getGif(strings.Join(parts[3:], "%20"))
+					postMessage(ws, m)
+				}(m)
+			} else {
+				// huh?
+				m.Text = fmt.Sprintf("sorry, that does not compute\n")
+				postMessage(ws, m)
+			}
+		}
+	}
+}
+
+// Get the quote via Yahoo. You should replace this method to something
+// relevant to your team!
+func getQuote(sym string) string {
+	sym = strings.ToUpper(sym)
+	url := fmt.Sprintf("http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=nsl1op&e=.csv", sym)
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Sprintf("error: %v", err)
+	}
+	rows, err := csv.NewReader(resp.Body).ReadAll()
+	if err != nil {
+		return fmt.Sprintf("error: %v", err)
+	}
+	if len(rows) >= 1 && len(rows[0]) == 5 {
+		return fmt.Sprintf("%s (%s) is trading at $%s", rows[0][0], rows[0][1], rows[0][2])
+	}
+	return fmt.Sprintf("unknown response format (symbol was \"%s\")", sym)
+}
+
+func getGif(sym string) string {
+	sym = strings.ToUpper(sym)
+	url := fmt.Sprintf("https://api.giphy.com/v1/gifs/search?api_key=21d84d49e2d8418a848a0e956efe5faa&q=%s&limit=1", sym)
+	resp, err := http.Get(url)
+	raw, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Sprintf("error: %v", err)
+	}
+	type Result struct {
+		Url string `json:"url"`
+	}
+	type Results struct {
+		Data []Result `json:"data"`
+	}
+	var data Results
+	if err = json.Unmarshal(raw, &data); err != nil {
+		return fmt.Sprintf("error: %v", err)
+	}
+	return fmt.Sprintf(data.Data[0].Url)
+}
